b2b66bffc2e38fb10218e42cb4062262b5cd8bae
Vasco Nunes <vbnunes@criticalsoftware.com>
2016-05-20 17:57:08 +0100

    altitude+pressão e ini file para definir Pythonpath

--- csxp1_collectdata.py
+++ csxp1_collectdata.py
@@ -2,20 +2,39 @@ from grovepi import *
 import csv
 import datetime
 import time
+import smbus
+import RPi.GPIO as GPIO
+from grove_i2c_barometic_sensor_BMP180 import BMP085
 
+# Initialise the BMP085 and use STANDARD mode (default value)
+# bmp = BMP085(0x77, 0)  # ULTRALOWPOWER Mode
+bmp = BMP085(0x77, 1)  # STANDARD Mode
+# bmp = BMP085(0x77, 2)  # HIRES Mode
+# bmp = BMP085(0x77, 3)  # ULTRAHIRES Mode
+
+# Initialise the dht sensor
 dht_sensor_port = 7
+
+# set bus port number according to rPI version
+rev = GPIO.RPI_REVISION
+if rev == 2 or rev == 3:
+    bus = smbus.SMBus(1)
+else:
+    bus = smbus.SMBus(0)
 
 def main():
 	with open('/data.csv', 'w') as fp:
 		a = csv.writer(fp, delimiter=',')
-		data = [["Timestamp","Temperatura", "Humidade"]]
+		data = [["Timestamp","Temperatura", "Humidade","Pressure","Altitude"]]
 		a.writerows(data)	
 		while True:
 			try:
-				[temp,hum]=dht(dht_sensor_port,1)		
+				[temp,hum]=dht(dht_sensor_port,1)
+				pressure = bmp.readPressure()
+				altitude = bmp.readAltitude(101560)
 				ts=datetime.datetime.now()
-				data = [[ts, temp, hum]]			
-				print "temperature = ", temp, " humidity = ", hum
+				data = [[ts, temp, hum, pressure, altitude]]			
+				print "temperature = ", temp, " humidity = ", hum, " pressure = ", pressure, " altitude = ", altitude
 				if isinstance(temp,float) and isinstance(hum,float):
 					a.writerows(data)
 				time.sleep(5)
